f05f8ad6e0587d67fcf77c47572983e75fa9f8dd
Philip Maina <maina.zak@nobilisltd.co.ke>
2021-03-16 00:09:45 +0300

    feat (devops): Added initial docker-compose.yml file.

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,7 @@
+# Don't buffer output to stdout as this doesn't play well with Docker Compose
+# (data written to stdout, e.g. via puts, may not be displayed on the terminal)
+$stdout.sync = true
+
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
